36b2bfa628ca523e03a0cceee0c6ff120461ebac
Dymitr Dragalow <dymitr.dragalow@comarch.com>
2021-08-16 12:49:35 +0200

    Commit

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use Flasher\Notyf\Prime\NotyfFactory;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
 
 use App\Entity\Product;
 use App\Entity\Category;
@@ -15,39 +19,192 @@ class ProductController extends AbstractController
     /**
      * @Route("/", name="app_products")
      */
-    public function index(): Response{
-        return $this->getProducts();
+    public function index(Request $request): Response{
+        return $this->getProducts($request);
     }
 
     /**
      * @Route("/products", name="get_products")
      */
-    public function getProducts(): Response
+    public function getProducts(Request $request): Response
     {
+        $id = $request->query->get('category');
+        if($id != null && $id != 0){
+            return $this->getFilteredProducts($id);
+        }else{
+            $repository = $this->getDoctrine()->getRepository(Product::class);
+
+            $products = $repository->findAll();
+
+            $repo =  $this->getDoctrine()->getRepository(Category::class);
+            $categories = $repo->findAll();
+
+            return $this->render('product/index.html.twig', [
+                'products' => $products,
+                'categories' => $categories,
+                'categoryId' => 0
+            ]);
+        }
+    }
+
+    /**
+     * @Route("/products", name="get_filtered_products")
+     */
+    public function getFilteredProducts($id): Response
+    {
+        $repository = $this->getDoctrine()->getRepository(Product::class);
+
+        $repo =  $this->getDoctrine()->getRepository(Category::class);
+        $categories = $repo->findAll();
+
+        $products = $repository->findByCategoryId($id);
+
+
         return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
+            'products' => $products,
+            'categories' => $categories,
+            'categoryId' => $id
         ]);
     }
 
     /**
      * @Route("/add-product", name="create_product")
+     * @IsGranted("ROLE_ADMIN")
      */
-    public function createProduct(): Response
+    public function createProduct(Request $request, NotyfFactory $flasher): Response
     {
-        // you can fetch the EntityManager via $this->getDoctrine()
-        // or you can add an argument to the action: createProduct(EntityManagerInterface $entityManager)
-        $entityManager = $this->getDoctrine()->getManager();
+        $error = null;
+        $name = "";
+        $description = "";
+        $price = 0;
+        $category = null;
 
-        $category = $this->getDoctrine()->getRepository(Category::class)->find(1);
+        $repo =  $this->getDoctrine()->getRepository(Category::class);
+        $categories = $repo->findAll();
 
-        $product = new Product("Ball", 12.99, "Fine ball", $category);
+        if($request->getMethod() == Request::METHOD_POST){
 
-        // tell Doctrine you want to (eventually) save the product (no queries yet)
-        $entityManager->persist($product);
+            $name = $request->request->get("name");
+            $description = $request->request->get("description");
+            $price = $request->request->get("price");
+            $category = $request->request->get("category");
 
-        // actually executes the queries (i.e. the INSERT query)
-        $entityManager->flush();
+            if (trim($name, " ") == "" || trim($description, " ") == "" || $price <= 0) {
+                $error = "Please fill the necessary fields!";
+            } else {
+                $entityManager = $this->getDoctrine()->getManager();
 
-        return new Response('Saved new product with id '.$product->getId());
+                $cat = $repo->find($category);
+                $product = new Product($name, $price, $description, $cat);
+
+
+                $entityManager->persist($product);
+                $entityManager->flush();
+
+                $flasher->addSuccess('Product has been added successfully!');
+                $name = "";
+                $description = "";
+                $price = 0;
+            }
+        }
+
+        return $this->render('product/add.html.twig', [
+            'error' => $error,
+            'name' => $name,
+            'description' => $description,
+            'price' => $price,
+            'categoryId' => $category,
+            'categories' => $categories
+        ]);
     }
+
+    /**
+     * @Route("/edit-product/{id}", name="edit_product", requirements={"id": "\d+"})
+     * @IsGranted("ROLE_ADMIN")
+     */
+    public function editProduct(Request $request, Product $product, NotyfFactory $flasher): Response
+    {
+        $error = null;
+        $name = "";
+        $description = "";
+        $price = 0;
+        $categoryId = null;
+
+        $repo =  $this->getDoctrine()->getRepository(Category::class);
+        $categories = $repo->findAll();
+
+        if($request->getMethod() == Request::METHOD_GET){
+
+            $name = $product->getName();
+            $description = $product->getDescription();
+            $price = $product->getPrice();
+            $categoryId = $product->getCategory()->getId();
+
+        }else if($request->getMethod() == Request::METHOD_POST){
+
+            $name = $request->request->get("name");
+            $description = $request->request->get("description");
+            $price = $request->request->get("price");
+            $categoryId = $request->request->get("category");
+
+            if (trim($name, " ") == "" || trim($description, " ") == "" || $price <= 0) {
+                $error = "Please fill the necessary fields!";
+            } else {
+
+                $entityManager = $this->getDoctrine()->getManager();
+
+                $category = $repo->find($categoryId);
+
+                $product->setName($name);
+                $product->setDescription($description);
+                $product->setPrice($price);
+                $product->setCategory($category);
+                $entityManager->flush();
+
+                $flasher->addSuccess('Data has been saved successfully!');
+                return $this->redirectToRoute('app_products');
+            }
+        }
+
+        return $this->render('product/edit.html.twig', [
+            'error' => $error,
+            'name' => $name,
+            'description' => $description,
+            'price' => $price,
+            'categories' => $categories,
+            'categoryId' => $categoryId
+        ]);
+    }
+
+    /**
+     * @Route("/delete-product/{id}", name="delete_product", requirements={"id": "\d+"})
+     * @IsGranted("ROLE_ADMIN")
+     */
+    public function deleteProduct(Request $request, Product $product, NotyfFactory $flasher): Response{
+
+        if($request->getMethod() == Request::METHOD_GET) {
+
+            $name = $product->getName();
+            $description = $product->getDescription();
+            $price = $product->getPrice();
+            $category = $product->getCategory()->getName();
+
+            return $this->render('product/delete.html.twig', [
+                'name' => $name,
+                'description' => $description,
+                'price' => $price,
+                'category' => $category
+            ]);
+
+        }else{
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($product);
+            $entityManager->flush();
+
+            $flasher->addSuccess('Data has been deleted successfully!');
+            return $this->redirectToRoute('app_products');
+        }
+    }
+
 }
--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -19,22 +19,19 @@ class ProductRepository extends ServiceEntityRepository
         parent::__construct($registry, Product::class);
     }
 
-    // /**
-    //  * @return Product[] Returns an array of Product objects
-    //  */
-    /*
-    public function findByExampleField($value)
+     /**
+      * @return Product[] Returns an array of Product objects
+      */
+    public function findByCategoryId($value): array
     {
         return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
+            ->andWhere('p.category = :val')
             ->setParameter('val', $value)
             ->orderBy('p.id', 'ASC')
-            ->setMaxResults(10)
             ->getQuery()
             ->getResult()
         ;
     }
-    */
 
     /*
     public function findOneBySomeField($value): ?Product
